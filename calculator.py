9c9a7457a7ddda4760552f8baea4e636b801ce0b
Henri Renzelmann <henrirpunkt@web.de>
2021-11-23 20:27:52 +0100

    adding curve fit for grad-calculation

--- calculator.py
+++ calculator.py
@@ -3,21 +3,44 @@ calculates all values
 """
 
 import numpy as np
+from scipy.optimize import curve_fit
+
+
+def func(x, a, b):
+    return 10**3*a*x+10**3*b
+
+def linfit(p, T):
+    p_new = []
+    T_new = []
+    for i in range(0, len(p)):
+        if p[i] < -750:
+            p_new.append(p[i])
+            T_new.append(T[i])
+    linear_model = np.polyfit(T_new,p_new,1)
+    p_of_T = np.poly1d(linear_model)
+    T_new = np.linspace(T_new[0], T_new[-1], num=6)
+    p_new = np.linspace(p_of_T(T_new[0]), p_of_T(T_new[-1]), num=6)
+    """print("type: ", type(p), type(p_new))
+    popt, pcov = curve_fit(func, T_new, p_new, bounds=([0, -10], [2.5, 0]))
+    print("fit: ", popt)
+    for j in range(0, len(p_new)):
+        p_new[j] = 10**3 * (popt[0] * T_new[j] + popt[1])
+    # p_new = popt[0] * T_new + popt[1]"""
+    return p_new, T_new
 
 
 def grad(depth, T):
     grads = []
 
+    f = open("test.txt", "w")
     for i in range(0, len(T)-1):
         dT = T[i+1] - T[i]
         dh = depth[i+1] - depth[i]
-        #print("dh: ", dh)
         grad = dT / dh
+        f.write("{b}: {a}\n".format(a=grad, b=depth[i]))
         grads.append(grad)
 
-    #print(grads)
     grads_mean = np.mean(grads)
-    #print(grads_mean)
     return grads_mean
 
 
--- newmainfunc.py
+++ newmainfunc.py
@@ -9,13 +9,15 @@ import calculator
 
 from matplotlib import pyplot as plt
 import numpy as np
+import os.path
 
 
 def main():
     ID = readin.overviewreader(-60, -58, -130, -128)
     print(ID[5800])
     for i in ID:
-        p_prof = readin.datareader_nc("{split}/profiles/D{full}.nc".format(split=i.split("_")[0], full=i), "PRES_ADJUSTED")[0]
+        """p_prof = readin.datareader_nc("{split}/profiles/D{full}.nc".format(split=i.split("_")[0], full=i), "PRES_ADJUSTED")[0]
+        p_prof = -p_prof
         T_prof = readin.datareader_nc("{split}/profiles/D{full}.nc".format(split=i.split("_")[0], full=i), "TEMP_ADJUSTED")[0]
         counter = 0
         for j in range(0, len(T_prof)):
@@ -26,8 +28,24 @@ def main():
         grad = calculator.grad(p_prof, T_prof)
         print("grad: ", grad)
         print("p: ", len(p_prof))
-        print("T: ", T_prof)
-    
+        print("T: ", T_prof)""" # Bitte _ADJUSTED nehmen, sobald geklärt wie mit fehlenden Werten umzugehen
+        p_prof = readin.datareader_nc("{split}/profiles/D{full}.nc".format(split=i.split("_")[0], full=i), "PRES")[0]
+        p_prof = -p_prof
+        T_prof = readin.datareader_nc("{split}/profiles/D{full}.nc".format(split=i.split("_")[0], full=i), "TEMP")[0]
+        # plot the measuremnts of the profile
+        plt.plot(T_prof, p_prof)
+        p_prof, T_prof = calculator.linfit(p_prof, T_prof)
+        grad = calculator.grad(p_prof, T_prof)
+        print("grad: ", grad)
+        #print("T: ", T_prof)
+        # plot the curve fit
+        plt.plot(T_prof, p_prof)
+        plt.savefig(os.path.join("plots", "{a}th_test".format(a=i)))
+        plt.show()
+    """
+    f = open("test.txt", "w")
+    for j in range(0, len(T_prof)):
+        f.write("{b}: {a}\n".format(a=T_prof[j], b=p_prof[j]))"""
 
 if __name__ == "__main__":
     main()
